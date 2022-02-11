4fc7d81942b3dc7c4e3f5096fa775011bb18d9a0
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-02-11 19:43:14 +0400

    resolve heroky problem build app

--- index.ts
+++ index.ts
@@ -45,8 +45,7 @@ const POST = (request:IncomingMessage, response:ServerResponse) => {
 
 const GET = (request:IncomingMessage, response:ServerResponse) => {
   response.writeHead(200, {'Content-Type': 'text/html',});
-  response.write(`<p>${request.url}</p>`);
-  response.end();
+  response.end("<p>HELLO!</p>");
 }
 
 const DEFAULT = (response:ServerResponse) => {
